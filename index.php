61af1b9140b1dbf0095a5967fb21e48e905df3e2
Nathan <Nathan@example.com>
2022-01-26 16:44:00 +0100

    Dev page accueil lien valide du menu

--- index.php
+++ index.php
@@ -3,6 +3,10 @@ require('Controllers/MeteoController.php');
 require('Controllers/MesureController.php');
 require('Controllers/LieuController.php');
 
+
+
+
+
 if(isset($_GET["page"])){
 
     require('view/'.$_GET["page"].'.php');
