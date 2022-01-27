453a25b3ac9e838f926c0f13a84d7782750638dc
Ramzer <71775283+Ramzer@users.noreply.github.com>
2022-01-27 11:55:36 +0100

    mesuremodel

--- Models/MesureModel.php
+++ Models/MesureModel.php
@@ -26,17 +26,17 @@ class MesureModel extends Database {
         $sqlQuery = 'SELECT * FROM mesure JOIN lieu ON mesure.id_lieu = lieu.id_lieu WHERE libelle_lieu = :lieu AND date_mesure = :date_mesure';
         $req =$this->db->prepare($sqlQuery);
         $req->execute(['lieu' => $libelleLieu, 'date_mesure'=> $date_mesure]);
-        $data = $req->fetchAll();
+        $data = $req->fetch(PDO::FETCH_ASSOC);
 
         return $data;
 
     }
 
-    public function Ajouter_Mesure ($temp, $temp_ressentie, $pression, $humidite, $temp_min, $temp_max, $vit_vent, $dir_vent, $rafale_vent, $nebulosite, $pluie_h, $pluie_3h, $leve_sol, $couche_sol, $date_mesure, $heure_mesure, $id_lieu, $id_meteo) {
+    public function Ajouter_Mesure ($temp, $temp_ressentie, $pression, $humidite, $vit_vent, $dir_vent, $rafale_vent, $nebulosite,$visibilite, $pluie_h, $pluie_3h, $leve_sol, $couche_sol, $date_mesure, $heure_mesure, $id_lieu, $id_meteo) {
 
        
-        $sqlQuery = 'INSERT INTO mesure(temperature, temperature_res, pression, humidite, temp_min_ville, temp_max_ville, vitesse_vent, direction_vent, rafale_vent, nebulosite, pluie_h, pluie_3h, leve_soleil, couche_soleil, date_mesure, heure_mesure, id_lieu, id_meteo) 
-                     VALUES            (:temperature, :temperature_res, :pression, :humidite, :temp_min_ville, :temp_max_ville, :vitesse_vent, :direction_vent, :rafale_vent, :nebulosite, :pluie_h, :pluie_3h, :leve_soleil, :couche_soleil, :date_mesure, :heure, :id_lieu, :id_meteo)';
+        $sqlQuery = 'INSERT INTO mesure(temperature, temperature_res, pression, humidite, vitesse_vent, direction_vent, rafale_vent, nebulosite, visibilite, pluie_h, pluie_3h, leve_soleil, couche_soleil, date_mesure, heure_mesure, id_lieu, id_meteo) 
+                     VALUES            (:temperature, :temperature_res, :pression, :humidite, :vitesse_vent, :direction_vent, :rafale_vent, :nebulosite, :visibilite, :pluie_h, :pluie_3h, :leve_soleil, :couche_soleil, :date_mesure, :heure, :id_lieu, :id_meteo)';
         
         $req = $this->db->prepare($sqlQuery);
 
@@ -44,13 +44,12 @@ class MesureModel extends Database {
             'temperature' => $temp,
             'temperature_res' => $temp_ressentie,
             'pression' => $pression,
-            'humidite' => $humidite, 
-            'temp_min_ville' => $temp_min, 
-            'temp_max_ville' => $temp_max, 
+            'humidite' => $humidite,  
             'vitesse_vent' => $vit_vent,
             'direction_vent' => $dir_vent,
             'rafale_vent' => $rafale_vent,
             'nebulosite' => $nebulosite,
+            'visibilite' => $visibilite,
             'pluie_h' => $pluie_h,
             'pluie_3h' => $pluie_3h,
             'leve_soleil' => $leve_sol,
@@ -58,8 +57,7 @@ class MesureModel extends Database {
             'date_mesure' => $date_mesure,
             'heure' => $heure_mesure,
             'id_lieu' => $id_lieu,
-            'id_meteo' => $id_meteo,
-        
+            'id_meteo' => $id_meteo,       
         ]);
 
         return $req;
